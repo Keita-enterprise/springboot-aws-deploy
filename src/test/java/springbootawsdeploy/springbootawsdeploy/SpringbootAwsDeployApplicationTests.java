16118e39cfabf7aa418f65ed7d70c5e21efa178a
i2bkeit <keibk@protonmail.com>
2023-10-05 20:10:25 -0400

    first commit

---
+++ src/test/java/springbootawsdeploy/springbootawsdeploy/SpringbootAwsDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootawsdeploy.springbootawsdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAwsDeployApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
